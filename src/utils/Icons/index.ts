41754e09ad3711a11826f8a28e4dc59aa06ba3f8
Saksham-garg <sakshamgarg142@hotmail.com>
2025-05-02 16:07:54 +0530

    added sidebar icons

---
+++ src/utils/Icons/index.ts
@@ -0,0 +1,17 @@
+import DashboardIcon from "./DashboardIcon";
+import AccountingIcon from "./AccountingIcon";
+import BankingIcon from "./BankingIcon";
+import InvoicingIcon from "./InvoicingIcon";
+import QueriesIcon from "./QueriesIcon";
+import ReportsIcon from "./ReportsIcon";
+import ContactsIcon from "./ContactsIcon";
+
+export {
+    DashboardIcon,
+    AccountingIcon,
+    BankingIcon,
+    InvoicingIcon,
+    QueriesIcon,
+    ReportsIcon,
+    ContactsIcon,
+}
